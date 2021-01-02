746bc6cf1135b19188dd28f2d79adaa9bb98ac2f
mdmohsin7 <59914433+mdmohsin7@users.noreply.github.com>
2021-01-02 17:43:25 +0530

    added url validator

--- lib/business/utils/validators.dart
+++ lib/business/utils/validators.dart
@@ -1,4 +1,3 @@
-
 String validateEmail(String value) {
   Pattern pattern =
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
@@ -9,12 +8,23 @@ String validateEmail(String value) {
     return null;
 }
 
-String validateYear(String value){
+String validateYear(String value) {
   Pattern pattern = r'^[2][0]{1}[1-2]{1}[0-9]{1}$';
   RegExp regExp = RegExp(pattern);
-  if(!regExp.hasMatch(value)){
+  if (!regExp.hasMatch(value)) {
     return 'invalid year';
-  }else{
+  } else {
+    return null;
+  }
+}
+
+String validateURL(String value) {
+  Pattern pattern =
+      r"(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&=]*)";
+  RegExp regExp = RegExp(pattern);
+  if (!regExp.hasMatch(value)) {
+    return "Invalid url.";
+  } else {
     return null;
   }
 }
